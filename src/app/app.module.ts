e9928f5e2e89a0d03ea860d893b720380bb0d553
Natasha Nadia <abc@gmail.com>
2022-03-07 07:23:44 +0800

    This is simple Covid19 API that use Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
+import { CoronaService } from './corona.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [CoronaService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
