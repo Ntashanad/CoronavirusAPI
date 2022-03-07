4b77e896de75f79cc7d7b4bc76490e4601bc3232
Ntashanad <96516285+Ntashanad@users.noreply.github.com>
2022-03-07 13:15:39 +0800

    Update corona.service.ts

--- src/app/corona.service.ts
+++ src/app/corona.service.ts
@@ -11,11 +11,13 @@ export class CoronaService {
 
   constructor(private http:HttpClient){ }
 
+  //to get data list for countries
   getCountries():Observable<any>{
     const url = "https://api.covid19api.com/countries" //covid api for countries
     return this.http.get<any>(url)
   }
 
+  //to get total day one data by country
   getCoronaRealtimeData(country):Observable<any>{
     const url = "https://api.covid19api.com/total/dayone/country/"+ country  //covid api for total day one
     return this.http.get<any>(url)
